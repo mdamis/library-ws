8804d60fae546a51d4bcdee62457ccaa77f8094e
Peroumalle <mperouma@gmail.com>
2015-12-11 09:15:07 -0800

    Add documentation to Book.java

--- WS/server/src/fr/upem/book/Book.java
+++ WS/server/src/fr/upem/book/Book.java
@@ -32,6 +32,14 @@ public class Book {
 		introductionDate = LocalDate.now();
 	}
 
+	/**
+	 * 
+	 * @param isbn the book's isbn
+	 * @param title the book's title
+	 * @param author the book's author name
+	 * @param price the book's price
+	 * @param introductionDate the book's introductionDate
+	 */
 	public Book(String isbn, String title, String author, float price, LocalDate introductionDate) {
 		this.isbn = isbn;
 		this.title = title;
@@ -40,6 +48,15 @@ public class Book {
 		this.introductionDate = introductionDate;
 	}
 
+	/**
+	 * 
+	 * @param isbn the book's isbn
+	 * @param title the book's title
+	 * @param author the book's author name
+	 * @param price the book's price
+	 * @param introductionDate the book's introductionDate
+	 * @return an instance of Book
+	 */
 	public static Book create(String isbn, String title, String author, float price, String introductionDate) {
 		LocalDate date;
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
@@ -50,19 +67,36 @@ public class Book {
 		}
 		return new Book(isbn, title, author, price, date);
 	}
-
+	
+	/**
+	 * 
+	 * @return the book's isbn
+	 */
 	public String getISBN() {
 		return isbn;
 	}
 
+	/**
+	 * 
+	 * @return the title
+	 */
 	public String getTitle() {
 		return title;
 	}
 
+	/**
+	 * 
+	 * @return the author
+	 */
 	public String getAuthor() {
 		return author;
 	}
 	
+	/**
+	 * Add exemplary of this book
+	 * @param exemplary number of book added
+	 * @throws IllegalArgumentException when the number to add is invalid
+	 */
 	public void addExemplary(int exemplary) throws IllegalArgumentException {
 		if(exemplary <= 0) {
 			throw new IllegalArgumentException("Invalid number of exemplary to add");
@@ -71,6 +105,11 @@ public class Book {
 		}
 	}
 	
+	/**
+	 * removed some exemplary of this book
+	 * @param exemplary the number of exemplary removed
+	 * @throws IllegalArgumentException when exemplary is invalid
+	 */
 	public void removeExemplary(int exemplary) throws IllegalArgumentException {
 		if(exemplary > nbExemplary || exemplary <= 0) {
 			throw new IllegalArgumentException("Invalid number of exemplary to remove");
@@ -79,6 +118,10 @@ public class Book {
 		}
 	}
 
+	/**
+	 * test if an exemplary is still available
+	 * @return true if book is available else false
+	 */
 	public boolean isAvailable() {
 		//return available;
 		return nbExemplary > 0;
@@ -100,10 +143,18 @@ public class Book {
 		this.hasBeenBorrowed = hasBeenBorrowed;
 	}*/
 
+	/**
+	 *  test if we can sell this book
+	 * @return true if this book is sellable else false
+	 */
 	public boolean isSaleable() {
 		return /*hasBeenBorrowed && LocalDate.now().getYear() -*/ introductionDate.getYear() >= 2;
 	}
 
+	/**
+	 * Get information of this book
+	 * @return a string representation describing the book
+	 */
 	public String details() {
 		if (isAvailable()) {
 			return title + " written by " + author + " is available";
@@ -112,14 +163,26 @@ public class Book {
 		}
 	}
 
+	/**
+	 * 
+	 * @return the summary of this book
+	 */
 	public String getSummary() {
 		return summary;
 	}
 
+	/**
+	 * 
+	 * @param summary set the summary
+	 */
 	public void setSummary(String summary) {
 		this.summary = summary;
 	}
 
+	/**
+	 * 
+	 * @return a String representation of all the review
+	 */
 	public String getReviews() {
 		if(reviews.size() == 0) {
 			return "No reviews for : " + title;
@@ -135,22 +198,42 @@ public class Book {
 		return sb.toString();
 	}
 
+	/**
+	 * 
+	 * @param review add the review to this book
+	 */
 	public void addReview(String review) {
 		reviews.add(review);
 	}
 	
+	/**
+	 * 
+	 * @param newPrice set a newPrice
+	 */
 	public void setPrice(float newPrice) {
 		price = newPrice;
 	}
 
+	/**
+	 * 
+	 * @return get the book's price
+	 */
 	public float getPrice() {
 		return price;
 	}
 
+	/**
+	 * 
+	 * @return the currency of the book
+	 */
 	public String getCurrency() {
 		return currency;
 	}
 	
+	/**
+	 * 
+	 * @return the numbre of exemplary of this book
+	 */
 	public int getExemplary() {
 		return nbExemplary;
 	}
