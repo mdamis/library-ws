5201d9a0bd26b45d3b842c995d1f65a6521201a1
Peroumalle <mperouma@gmail.com>
2015-12-11 05:38:28 -0800

    Change location of function converter

--- WS/client/src/fr/upem/main/Main.java
+++ WS/client/src/fr/upem/main/Main.java
@@ -1,28 +1,9 @@
 package fr.upem.main;
 
-import java.rmi.RemoteException;
-
 import javafx.application.Application;
-
-import javax.xml.rpc.ServiceException;
-
-import net.restfulwebservices.www.DataContracts._2008._01.Currency;
-import net.restfulwebservices.www.DataContracts._2008._01.CurrencyCode;
-import net.restfulwebservices.www.ServiceContracts._2008._01.CurrencyServiceLocator;
-import net.restfulwebservices.www.ServiceContracts._2008._01.ICurrencyService;
-import FaultContracts.GOTLServices._2008._01.DefaultFaultContract;
 import fr.upem.client.GUI;
 
 public class Main {
-
-	public static double getConvertRate(String fromCurrency, String toCurrency)
-			throws ServiceException, DefaultFaultContract, RemoteException, IllegalArgumentException {
-		ICurrencyService iCurrency = new CurrencyServiceLocator().getBasicHttpBinding_ICurrencyService();
-		Currency c = iCurrency.getConversionRate(CurrencyCode.fromString(fromCurrency),
-				CurrencyCode.fromString(toCurrency));
-		Double r = c.getRate();
-		return r;
-	}
 
 	public static void main(String[] args) {
 		try {
@@ -31,5 +12,4 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-
 }
