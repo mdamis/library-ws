60a1362570761b016536550745f2398ea9d826d1
Peroumalle <mperouma@gmail.com>
2015-12-11 09:13:15 -0800

    add Documentation to Bank and Account

--- WS/server/src/fr/upem/bank/Account.java
+++ WS/server/src/fr/upem/bank/Account.java
@@ -7,7 +7,10 @@ public class Account{
 	private final String name;
 	private final String firstname;
 	private final String currency;
-
+	
+	/**
+	 * Default constructor
+	 */
 	public Account() {
 		this.balance = 0;
 		this.number = 0;
@@ -16,6 +19,13 @@ public class Account{
 		this.currency = "EUR";
 	}
 
+	/**
+	 * Constructor more developped
+	 * @param number the account id
+	 * @param name the name of the owner
+	 * @param firstname the firstname of the owner
+	 * @param currency the currency chosen for the account
+	 */
 	public Account(int number, String name, String firstname, String currency) {
 		this.balance = 0;
 		this.currency = currency;
@@ -24,18 +34,42 @@ public class Account{
 		this.number = number;
 	}
 	
+	/**
+	 *  Factory method to create an Account
+	 * @param number the account id
+	 * @param name the name of the owner
+	 * @param firstname the firstname of the owner
+	 * @param currency the currency chosen for the account
+	 * @return an Account instance
+	 */
 	public static Account createAccount(int number, String name, String firstname, String currency){
 		return new Account(number, name, firstname, currency);
 	}
 
+	/**
+	 *  Currency getter
+	 * @return String:currency
+	 */
 	public String getCurrency() {
 		return currency;
 	}
 
+	/**
+	 *  test if the user exist
+	 * @param number the account id
+	 * @param name the name of the owner
+	 * @param firstname the firstname of the owner
+	 * @return true of false
+	 */
 	public boolean isValid(long number, String name, String firstname) {
 		return (this.number == number && this.name.equals(name) && this.firstname.equals(firstname));
 	}
 
+	/**
+	 * deposit an amount of money to the account
+	 * @param amount Double to deposit into account, must be positif
+	 * @return true if function sucessfully deposit else false
+	 */
 	public boolean deposit(double amount){
 		if (amount >= 0) {
 			balance += (amount);
@@ -46,7 +80,15 @@ public class Account{
 
 	}
 
+	/**
+	 * withdraw an amount of money from the account
+	 * @param amount Double to withdraw account, must be positif
+	 * @return true if function sucessfully withdraw else false
+	 */
 	public boolean withdrawal(double amount){
+		if (amount < 0) {
+			return false;
+		} 
 		if (authorizedPayment(amount)) {
 			balance -= (amount);
 			return true;
@@ -56,54 +98,59 @@ public class Account{
 
 	}
 
-	public boolean authorizedPayment(double amount) {
+	/**
+	 * function used to check if the amount doesn't break the balance
+	 */
+	private boolean authorizedPayment(double amount) {
 		return (balance - amount >= 0);
 	}
 
-	public double valueOfBalance() {
-		return balance;
-	}
-
+	/**
+	 * 
+	 * @return the balance of account
+	 */
 	public double getBalance() {
 		return balance;
 	}
 
+	/**
+	 * 
+	 * @param balance
+	 */
 	public void setBalance(double balance) {
 		this.balance = balance;
 	}
-
+	
+	/**
+	 * 
+	 * @return account ID
+	 */
 	public int getNumber() {
 		return number;
 	}
 
+	/**
+	 * 
+	 * @param number the new account ID
+	 */
 	public void setNumber(int number) {
 		this.number = number;
 	}
 
+	/**
+	 * 
+	 * @return the name of the owner
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * 
+	 * @return the firstname of the owner
+	 */
 	public String getFirstname() {
 		return firstname;
 	}
 
-	/*
-	 * public static void main(String[] args) throws DefaultFaultContract,
-	 * RemoteException, IllegalArgumentException, ServiceException { Account a =
-	 * new Account(); System.out.println(a.valueOfBalance());
-	 * System.out.println(a.getCurrency()); try { a.deposit(567, "JPY"); } catch
-	 * (DefaultFaultContract e) { // TODO Auto-generated catch block
-	 * e.printStackTrace(); } catch (RemoteException e) { // TODO Auto-generated
-	 * catch block e.printStackTrace(); } catch (IllegalArgumentException e) {
-	 * // TODO Auto-generated catch block e.printStackTrace(); } catch
-	 * (ServiceException e) { // TODO Auto-generated catch block
-	 * e.printStackTrace(); }
-	 * 
-	 * System.out.println(a.valueOfBalance());
-	 * System.out.println(a.getCurrency());
-	 * 
-	 * a.withdrawal(2); System.out.println(a.valueOfBalance());
-	 * a.withdrawal(700,"JPY"); System.out.println(a.valueOfBalance()); }
-	 */
 }
--- WS/server/src/fr/upem/bank/Bank.java
+++ WS/server/src/fr/upem/bank/Bank.java
@@ -13,10 +13,22 @@ public class Bank {
 		
 	}
 	
+	/**
+	 *  check if the accountID exist in the bank
+	 * @param accountID the accountId tested
+	 * @return true if the accountID is known else false
+	 */
 	public boolean exist(int accountID){
 		return mapAccount.containsKey(accountID);
 	}
 	
+	/**
+	 * Add an account to the Bank
+	 * @param name the name of the owner of the new account
+	 * @param firstname the firstname of the owner of the new account
+	 * @param currency the currency of the new account
+	 * @return the accountId automaticaly generated
+	 */
 	public int addAccount(String name,String firstname,String currency) {
 		int randomAccountId;
 	    do {
@@ -29,6 +41,11 @@ public class Bank {
 		return account.getNumber();
 	}
 	
+	/**
+	 * Remove an account from the Bank
+	 * @param accountNumber the account Id that will be removed
+	 * @return true if function sucessfuly removed account else false
+	 */
 	public boolean removeAccount(int accountNumber){
 		if(mapAccount.containsKey(accountNumber)){
 			mapAccount.remove(accountNumber);
@@ -36,11 +53,12 @@ public class Bank {
 		}
 		return false;
 	}
-	/*
-	public Account getAccount(int accountNumber) {
-		return mapAccount.get(accountNumber);
-	}
-	*/
+
+	/**
+	 * get string representation of the basic information of the account
+	 * @param accountId the account ID we get the detail from
+	 * @return a string representation of the basic information of the account
+	 */
 	public String getDetailAccount(int accountId){
 		Account account = mapAccount.get(accountId);
 		if(account!= null){
@@ -50,15 +68,25 @@ public class Bank {
 		}
 	}
 	
+	/**
+	 * get the currency of an account
+	 * @param accountId the accountID to seek
+	 * @return the currency of the Account, carefull it a blank String if no Currency was found
+	 */
 	public String getAccountCurrency(int accountId){
 		Account account= mapAccount.get(accountId);
 		if(account != null){
 			return account.getCurrency();
 		}else{
-			return null;
+			return "";
 		}
 	}
 	
+	/**
+	 * get the currency of an account
+	 * @param accountId the accountID to seek
+	 * @return the currency of the Account, carefull it return a blank String if no Currency was found
+	 */
 	public double getAccountBalance(int accountId){
 		Account account= mapAccount.get(accountId);
 		if(account != null){
@@ -68,6 +96,11 @@ public class Bank {
 		}
 	}
 	
+	/**
+	 * get the account id of an account
+	 * @param accountId the accountID to seek
+	 * @return the id of the Account, carefull it return 0 if no account was found was found
+	 */
 	public int getAccountId(int accountId){
 		Account account= mapAccount.get(accountId);
 		if(account != null){
@@ -77,6 +110,11 @@ public class Bank {
 		}
 	}
 	
+	/**
+	 * get the name's owner of an account
+	 * @param accountId the accountID to seek
+	 * @return the name's owner of the Account, carefull it return null if no Account was found
+	 */
 	public String getAccountName(int accountId){
 		Account account= mapAccount.get(accountId);
 		if(account != null){
@@ -86,6 +124,11 @@ public class Bank {
 		}
 	}
 	
+	/**
+	 * get the owner's firstname of an account
+	 * @param accountId the accountID to seek
+	 * @return owner's firstname of the Account, carefull it return null if no Account was found
+	 */
 	public String getAccountFirstName(int accountId){
 		Account account= mapAccount.get(accountId);
 		if(account != null){
@@ -95,6 +138,12 @@ public class Bank {
 		}
 	}
 	
+	/**
+	 * Deposit the amount of money to the an account
+	 * @param accountId the account Id of the account wanted
+	 * @param amount the amount deposite
+	 * @return true if the function sucessfuly deposit else false
+	 */
 	public boolean deposit(int accountId,double amount){
 		Account account= mapAccount.get(accountId);
 		if(account!= null) {
@@ -104,6 +153,12 @@ public class Bank {
 		}
 	}
 
+	/**
+	 * Withdraw the amount of money from the an account
+	 * @param accountId the account Id of the account wanted
+	 * @param amount the amount to withdraw
+	 * @return true if the function sucessfully withdraw else false
+	 */
 	public boolean withdrawal(int accountId,double amount){
 		Account account= mapAccount.get(accountId);
 		if(account!= null) {
@@ -112,10 +167,5 @@ public class Bank {
 			return false;
 		}
 	}
-	/*
-	public static void main(String[] args) {
-		Bank b = new Bank();
-		int id = b.addAccount("jo", "ko", "EUR");
-	}
-	*/
+
 }
